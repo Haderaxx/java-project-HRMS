efef040610f3a20845ffc1d2876c90f649832a05
Haderaxx <saarangudayan@gmail.com>
2025-09-15 01:22:17 +0530

    Update DBConnection.java

--- src/com/hrms/util/DBConnection.java
+++ src/com/hrms/util/DBConnection.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/hrms"; // your DB name
-    private static final String USER = "root"; // your MySQL username
-    private static final String PASSWORD = "3333"; // your MySQL password
+   private static final String URL = "jdbc:mysql://localhost:3306/hrms_db";
+private static final String USER = "hrms_user";
+private static final String PASSWORD = "Hrms@123";
 
     public static Connection getConnection() {
         try {
