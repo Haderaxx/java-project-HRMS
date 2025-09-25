cef2c250db523294b156bd46a7b73fdd70d43614
Haderaxx <saarangudayan@gmail.com>
2025-09-25 13:12:46 +0530

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,18 +1,15 @@
--- Retrieve all employees
+
 SELECT * FROM employees;
 
--- Retrieve all departments
 SELECT * FROM departments;
 
--- Retrieve all salaries
 SELECT * FROM salaries;
 
--- Retrieve employees with their department names
 SELECT e.first_name, e.last_name, d.department_name
 FROM employees e
 JOIN departments d ON e.department = d.department_name;
 
--- Retrieve employees with their salary details
 SELECT e.first_name, e.last_name, s.salary
 FROM employees e
 JOIN salaries s ON e.employee_id = s.employee_id;
+
