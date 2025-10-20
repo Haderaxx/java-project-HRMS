0f56be725a3e269d1f1cd96fffabe47d8b87c182
Haderaxx <saarangudayan@gmail.com>
2025-10-20 14:35:17 +0530

    Update AttendanceService.java

--- src/com/hrms/service/AttendanceService.java
+++ src/com/hrms/service/AttendanceService.java
@@ -5,14 +5,23 @@ import com.hrms.model.Attendance;
 import java.util.List;
 
 public class AttendanceService {
+
     private AttendanceDAO attendanceDAO = new AttendanceDAO();
 
+    // Mark attendance for multiple employees
     public void markAttendance(List<Attendance> attendanceList) {
-        attendanceDAO.saveAttendance(attendanceList);
+        for (Attendance att : attendanceList) {
+            attendanceDAO.markAttendance(att);
+        }
     }
 
-    public List<Attendance> getAttendanceRecords() {
-        return attendanceDAO.loadAttendance();
+    // Get all attendance records
+    public List<Attendance> getAllAttendance() {
+        return attendanceDAO.getAllAttendance();
+    }
+
+    // Get attendance for a specific employee
+    public List<Attendance> getAttendanceByEmployee(int empId) {
+        return attendanceDAO.getAttendanceByEmployee(empId);
     }
 }
-
