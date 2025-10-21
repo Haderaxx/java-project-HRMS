85b1453baad89fea0656126f679b0da558ac4340
Apoorv <24cs333@mgits.ac.in>
2025-10-21 23:37:16 +0530

    error fixed

--- src/com/hrms/model/Employee.java
+++ src/com/hrms/model/Employee.java
@@ -6,9 +6,16 @@ public class Employee {
     private String department;
     private double salary;
 
-    // Constructor
+    // Constructor with ID (used when fetching from DB)
     public Employee(int id, String name, String department, double salary) {
         this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Constructor without ID (used when adding new employees)
+    public Employee(String name, String department, double salary) {
         this.name = name;
         this.department = department;
         this.salary = salary;
@@ -55,7 +62,9 @@ public class Employee {
 
     // Test main method
     public static void main(String[] args) {
-        Employee emp = new Employee(1, "Apoor", "IT", 50000.0);
-        System.out.println(emp);
+        Employee emp1 = new Employee(1, "Apoor", "IT", 50000.0);
+        Employee emp2 = new Employee("John", "HR", 40000.0);
+        System.out.println(emp1);
+        System.out.println(emp2);
     }
 }
